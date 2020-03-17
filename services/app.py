d5e11565bd2e0402b2ea6a9d9d0afc9a50b639d3
warriorgithub <48765702+warriorgithub@users.noreply.github.com>
2020-03-17 14:25:37 +0530

    updated app.py

--- services/app.py
+++ services/app.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 from flask import jsonify
 from firebase import firebase
+from pandas.io.json import json_normalize
 
 
 app = Flask(__name__)
@@ -13,20 +14,21 @@ def matchInterest():
     module_url = "https://tfhub.dev/google/universal-sentence-encoder/4"
     model = hub.load(module_url)
     user = request.args.get('user')
-    data_local_raw = {'First_Name':['Marian', 'Joe', 'Gibbs', 'Linda', 'Max', 'Cesar', 'Liza', 'Amanda', 'Dian', 'Donald'], 
-             'Last_name':['Boynton', 'Vaz', 'Paul', 'Hundsay', 'Millan', 'Millar', 'Ray', 'Lopez', 'Harris', 'Obama'],
-            'Interest_1':['data science', 'Roller skating', 'Look at the clouds', 'mobile app development', 'Summer job', 'Go hunting', 'to become and entrepreneur', 'CRICKET', 'theatres and drama', 'Computer Vision'],
-            'Interest_2':['Python', 'reading thriller books', 'Horse races', 'deep learning in computer vision', 'Make bird feeder', 'Collect sea shells', 'Pick wild flowers', 'playing musical instruments', 'sharing my experience and wisdom', 'machine learning'],
-            'Interest_3':['c++', 'legged race', 'angel investor', 'Mini golf', 'Adopt a star', 'Watch a sunset', 'all things related to cats', 'connecting with kindred spirits', 'research and discovery', 'Data Science']}
-    data_local = pd.DataFrame(data_local_raw)
-    callFirebase()
+    print('user :::::::::: ',user)
+    # data_local_raw = {'First_Name':['Marian', 'Joe', 'Gibbs', 'Linda', 'Max', 'Cesar', 'Liza', 'Amanda', 'Dian', 'Donald'], 
+    #          'Last_name':['Boynton', 'Vaz', 'Paul', 'Hundsay', 'Millan', 'Millar', 'Ray', 'Lopez', 'Harris', 'Obama'],
+    #         'Interest_1':['data science', 'Roller skating', 'Look at the clouds', 'mobile app development', 'Summer job', 'Go hunting', 'to become and entrepreneur', 'CRICKET', 'theatres and drama', 'Computer Vision'],
+    #         'Interest_2':['Python', 'reading thriller books', 'Horse races', 'deep learning in computer vision', 'Make bird feeder', 'Collect sea shells', 'Pick wild flowers', 'playing musical instruments', 'sharing my experience and wisdom', 'machine learning'],
+    #         'Interest_3':['c++', 'legged race', 'angel investor', 'Mini golf', 'Adopt a star', 'Watch a sunset', 'all things related to cats', 'connecting with kindred spirits', 'research and discovery', 'Data Science']}
+    user_list = callFirebase()
+    data_local = pd.DataFrame.from_dict(user_list)
+    print('data------------------------->  ',data_local.head())
     return find_a_match(int(user) , data_local,model)
 
 def callFirebase():
-    
     fb = firebase.FirebaseApplication("https://interestmat.firebaseio.com/",None)
     data = fb.get("/user-list",'')
-    print('data------------------------->  ',data)
+    return data
 def embed(input,model):
   return model(input)
 
@@ -51,12 +53,14 @@ def calculate_corr_value(person1, person2,model):
     return sum
 
 def find_a_match(person_id , data_local,model):
+    
     person1_list = data_local.iloc[person_id, 2:].values.tolist()
     best_match_value = float('inf')
     best_match_index = 0
     result=[]
     res={}
     for j in range (10):
+        print('in JJJJJ a match :::::',j)
         if person_id != j:
             person2_list = data_local.iloc[j, 2:].values.tolist()
             corr = calculate_corr_value(person1_list, person2_list,model)
@@ -67,9 +71,10 @@ def find_a_match(person_id , data_local,model):
     result.append("\n{} {}({}) shares similar interest with {} {}({})".format(data_local.iloc[person_id, 0], 
         data_local.iloc[person_id, 1],person_id, data_local.iloc[best_match_index, 0],data_local.iloc[best_match_index,1], best_match_index))
 
-    res = { i : result[i] for i in range(0, len(result) ) }    
+    res = { i : result[i] for i in range(0, len(result) ) }  
+    print('in fina a match :::::',res)  
     return jsonify(res)
  
            
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0')
